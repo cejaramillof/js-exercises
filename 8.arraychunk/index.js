150e813083050deadbda5ce702304e491d017762
Carlos Jaramillo <cejaramillof@mercadolibre.com.co>
2022-02-14 22:00:22 -0500

    array chunk

--- 8.arraychunk/index.js
+++ 8.arraychunk/index.js
@@ -9,21 +9,23 @@
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
 function chunk(array, size) {
-//   const chunked = [];
-//
-//   for (let element of array) {
-//     const last = chunked[chunked.length - 1];
-//
-//     if (!last || last.length === size) {
-//       chunked.push([element]);
-//     } else {
-//       last.push(element);
-//     }
-//   }
-//
-//   return chunked;
-
   const chunked = [];
+
+  /*
+  for (let element of array) {
+    // const last = chunked.at(-1);
+    const last = chunked[chunked.length - 1];
+
+    if (!last || last.length === size) {
+      chunked.push([element]);
+    } else {
+      last.push(element);
+    }
+  }
+
+  return chunked;
+  */
+
   let index = 0;
 
   while (index < array.length) {
@@ -32,6 +34,7 @@ function chunk(array, size) {
   }
 
   return chunked;
+
 }
 
 module.exports = chunk;
